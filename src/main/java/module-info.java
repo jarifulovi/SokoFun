dfa6f2b62d5a4740a649d91f5a736d7c1b0c8c84
ovi <jarifulovi@gmail.com>
2025-08-29 19:43:29 +0600

    Update dependencies and clean up `pom.xml` and module-info
    - Standardized `artifactId` to lowercase (`sokofun`).
    - Upgraded JavaFX dependencies to version `21.0.2`.
    - Removed unused dependencies and corresponding module imports.
    - Organized `pom.xml` for better readability.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,6 @@ module com.puzzlegame.sokofun {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
     opens com.puzzlegame.sokofun to javafx.fxml;
     exports com.puzzlegame.sokofun;
     exports com.puzzlegame.sokofun.Controllers;
